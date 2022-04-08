20186c8e0dfeb147ba4457d5225f2a8a3696608a
harshitha <harshithar242000@gmail.com>
2022-04-09 04:47:40 +0530

    MVC

---
+++ src/Q8/springDispatcherInitializer.java
@@ -0,0 +1,21 @@
+package Q8;
+
+public class springDispatcherInitializer {
+	import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+	public class springDispatcherInitializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
+	    @Override
+	    protected Class<?>[] getRootConfigClasses() {
+	        return null;
+	    }
+
+	    @Override
+	    protected Class<?>[] getServletConfigClasses() {
+	        return new Class[]{AppConfig.class};
+	    }
+
+	    @Override
+	    protected String[] getServletMappings() {
+	        return new String[]{"/"};
+	    }
+	}
